f8c418cc2dd2043d3cc89c4853882af9e6c2726b
maxiturchet <71859870+maxiturchet@users.noreply.github.com>
2023-03-20 09:42:44 -0300

    Rutas y primeros componentes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import './App.css';
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import Home from './pages/ExerciseDetail';
+import ExerciseDetail from "./pages/Home";
+
 const App = () => {
     return(
-        <div>App</div>
+        <Box width='400px' sx={{ width: {xl: '1488px' }}} m='auto'>
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/exercise/:id" element={<ExerciseDetail />} />
+            </Routes>
+            <Footer />
+        </Box>
     )
 }
 
