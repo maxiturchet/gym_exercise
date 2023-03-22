67863c966ce3c9cb94120a4aebf8c5ccda9f0b90
maxiturchet <71859870+maxiturchet@users.noreply.github.com>
2023-03-22 15:25:48 -0300

    Finalizado

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
+  <React.StrictMode>
     <BrowserRouter>
-       <App/>
+      <App />
     </BrowserRouter>
+  </React.StrictMode>,
 );
+
